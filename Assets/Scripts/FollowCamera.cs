184d31fe0b09e13b6b2621863ac5dedd663fff72
FORT_KRENOX <rshisharin@mail.ru>
2023-09-17 12:11:33 +0300

    Update camera script

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class FollowCamera : MonoBehaviour
 {
-    /*
-    * A rectangle in which can
-    * move the camera.
-     */
+    /// <summary>
+    /// A rectangle in which can
+    /// move the camera. 
+    /// </summary>
     private Rect    boundary; 
     public Rect     Boundary
     {
@@ -15,48 +16,63 @@ public class FollowCamera : MonoBehaviour
     }
 
     [SerializeField] GameObject player;
-
-    /*
-    * Maximum possible distance
-    * between the player and the center of the camera.
-    * When going beyond its limit ,
-    * camera movement.
-     */
+    
+    /// <summary>
+    ///  Maximum possible distance
+    /// between the player and the center of the camera.
+    /// When going beyond its limit,
+    /// camera movement.
+    /// </summary>
     [SerializeField] float      maxDistance;
 
     Vector3                     offset;
-    float halfHeight;
-    float halfWidth;
 
-    void Start()
+    float                       halfHeight;
+    float                       halfWidth;
+
+    void Awake()
     {
         var camera =    Camera.main;
         halfHeight =    camera.orthographicSize;
         halfWidth =     camera.aspect * halfHeight;
+
+        var playerPosition = player.transform.position;
+        offset = transform.position - playerPosition;
     }
 
     void inBoundary()
     {
-        if ( transform.position.y - halfHeight < boundary.yMax  )
+        if ( transform.position.y - halfHeight < boundary.yMax )
+        { 
             transform.position = new Vector3( transform.position.x, boundary.yMax + halfHeight, -10 );
+        }
         if ( transform.position.y + halfHeight > boundary.yMin )
+        {
             transform.position = new Vector3( transform.position.x, boundary.yMin - halfHeight, -10 );
+        }
         if ( transform.position.x - halfWidth < boundary.xMin )
+        {
             transform.position = new Vector3( boundary.xMin + halfWidth, transform.position.y, -10 );
+        }
         if ( transform.position.x + halfWidth > boundary.xMax )
+        {
             transform.position = new Vector3( boundary.xMax - halfWidth, transform.position.y, -10 );
+        }
     }
 
-    private void Update()
-    {
-    }
     void LateUpdate()
     {
+        var playerPosition = player.transform.position;
+
         // Checking whether the player has gone beyond the maximum distance from the center camera
-        if ((( Vector2 )player.transform.position - ( Vector2 )transform.position ).sqrMagnitude <= maxDistance * maxDistance )
-            offset = transform.position - player.transform.position;
+        if (( playerPosition - transform.position ).sqrMagnitude < maxDistance * maxDistance * transform.position.z )
+        {
+            offset = transform.position - playerPosition;
+        }
         else
-            transform.position = ( offset + player.transform.position );
+        {
+            transform.position = offset + playerPosition;
+        }
         inBoundary();
     }
 }
